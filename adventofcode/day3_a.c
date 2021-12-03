f371fc0107549128ebebaeedc1e4dfe2fc118adf
larsh1997 <59018457+larsh1997@users.noreply.github.com>
2021-12-03 17:40:54 +0100

    Add files via upload

---
+++ adventofcode/day3_a.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+
+int binaryToInteger(int arr[], int sizeOf_array);
+
+int main() {
+
+    char *filename = "day3_data.txt";             //input data in txt file
+    FILE *fp = fopen(filename, "r");              //open file in read mode
+    const unsigned int arraySize = 12;            //12 digits binary numbers a given in the day 3 data
+    int gamma[arraySize];                         //Stores the gamma value as a binary number
+    int epsilon[arraySize];                       //Stores the epsilon value as a binary number
+    int most_common_bit; 
+    char *a;
+
+    if (fp == NULL)
+    {
+        printf("Error: could not open file %s", filename);
+        return 1;
+    }
+
+    const unsigned MAX_LENGTH = 256;
+    char buffer[MAX_LENGTH];
+
+        for (int i = 0; i < arraySize; i++){ 
+
+           while (fgets(buffer, MAX_LENGTH, fp)){
+            a = buffer;
+           
+            if(a[i] == '0'){
+                most_common_bit--;
+            } else {
+                most_common_bit++;
+            }
+           
+           //epsilon is just gamma inverted
+           if(most_common_bit > 0){
+            gamma[i] = 1;
+            epsilon[i] = 0;
+           } else {
+            gamma[i] = 0;
+            epsilon[i] = 1;
+           }
+
+        } 
+        most_common_bit = 0;            //reset most common bit for every iteration 
+        rewind(fp);                     //jump back to first address of the txt file
+
+   }
+    
+   
+
+    int result_gamma = binaryToInteger(gamma, arraySize);
+    int result_epsilon = binaryToInteger(epsilon, arraySize);
+    printf("result  %d \n", result_gamma * result_epsilon);  //print result
+
+    // close the file
+    fclose(fp);
+
+   return 0;
+}
+
+/*
+Converts a binary number, where each digit is stored in an single array cell to a decimal number
+
+@para      *arr             pointer to the array that holds the binary number
+           sizeOf_array     Size of said array
+
+@return                     Decimal number for the given binary
+*/
+int binaryToInteger(int *arr, int sizeOf_array){
+
+    int result = 0;
+    for(int k = 0; k < sizeOf_array; k++){
+        result += arr[k] * pow(2,(sizeOf_array-k-1));
+        printf("arr[k] %d und pow %d und reuslt %d \n",arr[k], (int)pow(2,(sizeOf_array-k-1)), result );
+    }
+    return result;
+
+}
---
+++ adventofcode/day3_b.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define MAX_LENGTH 256
+
+
+
+int binaryToInteger(int arr[], int sizeOf_array);
+void deleteLine(FILE *fp, FILE *fp_tmp, int val, int iteration);
+
+int main() {
+
+    FILE *fp = fopen("day3_data.txt", "r+");            //open file in read+ mode
+    FILE *fp_tmp = fopen("day3_tmp.txt", "r+");         //temporary txt file 
+    const int arraySize = 12;                           //12 digits binary numbers a given in the day 3 data
+    int firstNumber[arraySize];                         //Stores the gamma value as a binary number
+    int most_common_bit; 
+    char *a;
+    char val;
+    int iteration = 0;
+ 
+
+    if (fp == NULL || fp_tmp == NULL)
+    {
+        printf("Error: could not open file");
+        return 1;
+    }
+
+ 
+
+    char buffer[MAX_LENGTH];
+
+    int line_counter = 0;
+        for (int i = 0; i < 12; i++){ 
+        // while ( i<12){
+
+           while (fgets(buffer, MAX_LENGTH, fp) != NULL){
+            a = buffer;
+            
+            line_counter++;
+            if(a[i] == '0'){
+                most_common_bit--;  
+            } else {
+                most_common_bit++;  
+            }
+           
+            
+           if(most_common_bit >= 0){
+                val = '1';   //for oxygen generator rating keep it like this, for CO2 scrubber rating invert it
+           } else {  
+                val = '0';   //for oxygen generator rating keep it like this, for CO2 scrubber rating invert it
+           }
+        } 
+
+        if(line_counter == 1){
+
+            for(int e = 0; e < arraySize; e++){
+                firstNumber[e] = (int)(a[e]- '0'); //cast char to int
+            }
+            break;
+        }
+
+        line_counter = 0;
+        deleteLine(fp, fp_tmp, val, iteration);  //delete irrelevant lines
+        iteration++;
+        
+        most_common_bit = 0;            //reset most common bit for every iteration 
+        rewind(fp);                     //jump back to first address of the txt file 
+
+   }
+
+        int result = binaryToInteger(firstNumber, arraySize);
+        printf("%d ", result);
+    
+    // close the file
+    fclose(fp);
+    fclose(fp_tmp);
+
+   return 0;
+}
+
+/*
+Converts a binary number, where each digit is stored in an single array cell to a decimal number
+
+@para      *arr             pointer to the array that holds the binary number
+           arraySize     Size of said array
+
+@return                     Decimal number for the given binary
+*/
+int binaryToInteger(int *arr, int arraySize){
+
+    int result = 0;
+   
+    char str[2];
+    int val = 0;
+  
+            
+    for(int k = 0; k < arraySize; k++){
+        result += (arr[k] * (int)pow(2,(arraySize-k-1)));
+    }
+    return result;
+
+}
+
+/*
+Deletes the lines corresponding the the given val value e.g. if val = 1, it deletes
+the columns that contain a 0 
+
+@para           *fp         textfile 1
+                *fp_tmp     temporary textfile
+                val         either 1 or 0
+                iteration   corresponds to the current column (0-11)
+*/
+
+void deleteLine(FILE *fp, FILE *fp_tmp, int val, int iteration){
+
+
+    char buffer[MAX_LENGTH];
+    char *a;
+    
+    rewind(fp);
+    fclose(fp_tmp);
+    fp_tmp = fopen("day3_tmp.txt", "w+");
+
+    while(fgets(buffer, MAX_LENGTH, fp) != NULL){
+        a = buffer;
+
+        if(a[iteration] != val){ 
+            
+            fputs(buffer,fp_tmp);
+        }  
+    }
+
+    rewind(fp_tmp);
+
+    fclose(fp);
+    fp = fopen("day3_data.txt", "w+");  
+
+    while(fgets(buffer, MAX_LENGTH, fp_tmp) != NULL){
+            fputs(buffer,fp);
+    }
+
+    fclose(fp);
+    fp = fopen("day3_data.txt", "r+");
+
+}
+
